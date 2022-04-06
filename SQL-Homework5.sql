850f47a672fde683c135140c0bf4dda61584fcab
Hira Balcı <93313737+HiraBalci@users.noreply.github.com>
2022-04-06 11:09:53 +0300

    Create SQL-Homework5.sql

---
+++ SQL-Homework5.sql
@@ -0,0 +1,4 @@
+SELECT title,length FROM film WHERE title LIKE '%n' ORDER BY length DESC LIMIT 5; --1.sorunun cevabı
+SELECT title,length FROM film WHERE title LIKE '%n' ORDER BY length ASC OFFSET 5 LIMIT 5; --2.sorunun cevabı
+SELECT last_name,store_id FROM customer WHERE store_id=1 ORDER BY last_name DESC LIMIT 4;--3.sorunun cevabı
+
